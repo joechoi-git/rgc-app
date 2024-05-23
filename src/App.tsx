50991b9bb0f1f785903274e69c7ee056a0d7af8f
Joe Choi <joechoi.newyork@gmail.com>
2024-05-23 15:33:16 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./Dashboard";
 
 function App() {
     return (
         <div className="App">
+            <Dashboard />
+            {/*}
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
@@ -19,6 +22,7 @@ function App() {
                     Learn React!
                 </a>
             </header>
+            {*/}
         </div>
     );
 }

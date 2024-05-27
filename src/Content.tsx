819f3ed136a5dde2681dedf33c14bec231dbaae4
Joe Choi <joechoi.newyork@gmail.com>
2024-05-27 06:49:51 -0400

    update

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,53 +1,103 @@
 import * as React from "react";
 // eslint-disable-next-line
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Typography from "@mui/material/Typography";
 
 const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
-    {
-        field: "age",
-        headerName: "Age",
-        type: "number",
-        width: 90
-    },
-    {
-        field: "fullName",
-        headerName: "Full name",
-        description: "This column has a value getter and is not sortable.",
-        width: 160
-        // sortable: false,
-        // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`
-    }
+    { field: "id", headerName: "Concept ID", type: "number", width: 70 },
+    { field: "displayName", headerName: "Display Name", width: 130 },
+    { field: "description", headerName: "Description", width: 130 },
+    { field: "parentIds", headerName: "Parent IDs", width: 130 },
+    { field: "childIds", headerName: "Child IDs", width: 130 },
+    { field: "alternateNames", headerName: "Alternate Names", width: 130 }
 ];
 
 const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 }
+    {
+        id: 1,
+        displayName: "Diagnosis",
+        description: "Entity domain",
+        parentIds: "",
+        childIds: "2,3",
+        alternateNames: ""
+    },
+    {
+        id: 2,
+        displayName: "Disease of Nervous System",
+        description: "Diseases targeting the nervous system",
+        parentIds: "1",
+        childIds: "4",
+        alternateNames: ""
+    },
+    {
+        id: 3,
+        displayName: "Disease of Eye",
+        description: "Diseases targeting the eye",
+        parentIds: "1",
+        childIds: "8,9",
+        alternateNames: ""
+    },
+    {
+        id: 4,
+        displayName: "Physical Disorders",
+        description: "Physical Disorders",
+        parentIds: "1",
+        childIds: "8,9",
+        alternateNames: ""
+    },
+    {
+        id: 5,
+        displayName: "Multiple Sclerosis (MS)",
+        description: "Multiple Sclerosis",
+        parentIds: "2,4",
+        childIds: "5,6,7",
+        alternateNames: "MS,name1,name2"
+    }
 ];
 
 export default function Content() {
     return (
-        <div style={{ height: 400, width: "100%" }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 }
-                    }
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
-            />
-        </div>
+        <>
+            <Typography
+                component="h6"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+                className="pb-3"
+            >
+                Data Grid with Column Sorting and Pagination
+            </Typography>
+            <div style={{ height: 400, width: "100%" }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 }
+                        }
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection={false}
+                />
+            </div>
+            <Typography
+                component="h6"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+                className="pb-3 pt-10"
+            >
+                Relationship Representation
+            </Typography>
+            <div style={{ height: 400, width: "100%" }}>
+                {rows.map((row) => (
+                    <div key={row.id} style={{ padding: 10 }}>
+                        {row.displayName}
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }

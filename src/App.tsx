b9f4cbf7bd0822482ffdad6c6cada08a4ce4c667
Joe Choi <joechoi.newyork@gmail.com>
2024-05-27 10:16:57 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Link: <Link to="/">Dashboard</Link>
 import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 function App() {
-    return <Dashboard />;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
